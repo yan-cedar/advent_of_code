82d09dbda12b81752b0a45450ab4f9a5f0780fb7
Yan Zhou <yzhou@cedar.com>
2024-12-04 22:54:28 -0500

    day 3 complete

--- 2024/Day3/day-3.py
+++ 2024/Day3/day-3.py
@@ -1,55 +1,77 @@
-from urllib.parse import quote_plus
-
 input_file_name = 'day-3-input.txt'
 
 chars = open(input_file_name, 'r').read().split('\n')
 text = "".join(chars)
 
-result = 0
-
 key_start = 'mul('
 key_end = ')'
+enable_key = 'do()'
+disable_key = "don't()"
 
+### part 1
+def get_mul_range(text, start_idx, end_idx):
+    result = 0
 
-def has_space(string):
-    if " " in string:
-        return True
+    i = start_idx
 
-    return False
+    while i <= end_idx:
+        start = text.find(key_start, i, end_idx)
+        if start == -1:
+            break
 
+        end = text.find(key_end, start)
+        if end == -1:
+            break
 
-i = 0
+        target = text[start + len(key_start):end]
 
-while i < len(text):
-    start = text.find(key_start, i)
-    if start == -1:
-        break
+        try:
+            left, right = target.split(',')
+            result += int(left) * int(right)
+        except ValueError:
+            found = target.rfind(key_start)
 
-    end = text.find(key_end, start)
-    if end == -1:
-        break
+            if found != -1:
+                inner_found = target[found + len(key_start):end]
+                try:
+                    left, right = inner_found.split(',')
+                    if " " in left or " " in right:
+                        i = end + 1
+                        continue
 
-    target = text[start+len(key_start):end]
-
-    try:
-        left, right = target.split(',')
-        result += int(left) * int(right)
-    except ValueError:
-        found = target.rfind(key_start)
-
-        if found != -1:
-            inner_found = target[found+len(key_start):end]
-            try:
-                left, right = inner_found.split(',')
-                if has_space(left) or has_space(right):
+                    result += int(left) * int(right)
+                except ValueError:
                     i = end + 1
                     continue
 
-                result += int(left) * int(right)
-            except ValueError:
-                i = end + 1
-                continue
+        i = end + 1
 
-    i = end + 1
+    return result
 
-print('result: ', result)
+result = get_mul_range(text, 0, len(text)-1)
+print("part 1 result: ", result)
+
+### part 2
+i = 0
+res = 0
+
+while i < len(text):
+    disable_start = text.find(disable_key, i)
+    if disable_start == -1:
+        # no more disable key
+        res += get_mul_range(text, i, len(text)-1)
+        break
+
+    # found disable key
+    res += get_mul_range(text, i, disable_start-1)
+
+    i = disable_start + len(disable_key)
+    enable_start = text.find(enable_key, i)
+
+    if enable_start == -1:
+        # disabled till end
+        break
+    else:
+        i = enable_start + len(enable_key)
+
+print("part 2 result: ", res)
